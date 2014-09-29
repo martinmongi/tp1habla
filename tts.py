cdcbbbe2daeb9855ea816946cd1e8725ac3bac7f
Martín Mongi Badía <martinmongi@gmail.com>
2014-09-29 00:06:18 -0300

    arrancado el script

---
+++ tts.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+
+# ==== Parseo los argumentos ======================================================================
+args = sys.argv
+texto = args[1]
+out = args[2]
+
+print 'Texto de entrada:', texto
+print 'Archivo de salida:', out
+
+# ==== Me fijo si es pregunta, me voy a ocupar mas tarde ==========================================
+
+es_pregunta = (texto[-1] == '?')
+
+if es_pregunta: # si es pregunta, le saco el simbolo
+	texto = texto[:-1]
+
+# ==== Para cada par de letras, busco el difono ===================================================
+
+concat = ["./difonos/-" + texto[0] + ".wav"] # meto el primer difono
+
+for i in xrange(1,len(texto)):
+	concat.append("./difonos/" + texto[i-1:i+1] + ".wav")
+
+concat.append("./difonos/" + texto[-1] + "-.wav") # meto el ultimo difono
+
